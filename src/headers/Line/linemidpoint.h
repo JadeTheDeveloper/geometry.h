aa3a80adcbb1cad02e96d1241d061c6f8f1e4438
JadeTheDeveloper <cazextra@gmail.com>
2024-11-07 16:11:57 -0700

    Implemented mid point function

--- src/headers/Line/linemidpoint.h
+++ src/headers/Line/linemidpoint.h
@@ -2,15 +2,28 @@
 #include <stdlib.h>
 
 
-double *find_Line_Mid_Point(double point1[], double point2[]){
+struct linemidpoint {
+    double horizontalPoint;
+    double verticalPoint;
+};
+
+
+
+
+// * This should only take two args
+struct linemidpoint find_Line_Mid_Point(double point1[], double point2[]){
     
+    struct linemidpoint midpoint;
+
+    // The actual math
     double horizontalDifference = point1[0] - point2[0];
     double verticalDifference = point1[1] - point2[1];
 
     double horizontalDistace = horizontalDifference / 2;
     double verticalDistance = verticalDifference / 2;
 
-    double calculatedPoint[] = {horizontalDifference, verticalDistance}; 
-    
-    return calculatedPoint;
+    // ? Sets the struct return value
+    midpoint.horizontalPoint = horizontalDistace;
+    midpoint.verticalPoint = verticalDistance;
+    return midpoint;
 }
